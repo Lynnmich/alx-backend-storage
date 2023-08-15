4fd9a7156fb01e115d512d609d80f3752df23a73
Lynnmich <lynnenmichuki@gmail.com>
2023-08-15 13:03:54 +0300

    Task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,6 +2,6 @@
 """A Python function that lists all documents in a collection"""
 
 
-def list_all(mongo_collection)
+def list_all(mongo_collection):
     """Returns an empty list if no doc is in the collection"""
     return mongo_collection.find()
