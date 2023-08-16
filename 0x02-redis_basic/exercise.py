00359a13f6b9297ec41b37b7fa9d02c0d209a4e6
Lynnmich <lynnenmichuki@gmail.com>
2023-08-16 17:10:48 +0000

    Task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,7 @@ from typing import Any, Callable, Union
 
 
 def count_calls(method: Callable) -> Callable:
-    """Tracks the number of calls made to a method in a Cache class"""
+    """Keeps track of the number of calls made to a method in a Cache class"""
     @wraps(method)
     def callcounter(self, *args, **kwargs) -> Any:
         """Invokes the given method after incrementing its call counter"""
@@ -18,6 +18,22 @@ def count_calls(method: Callable) -> Callable:
             self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
     return callcounter
+
+def call_history(method: Callable) -> Callable:
+    """Keeps track of the call details of a method in a Cache class"""
+    @wraps(method)
+    def callcounter(self, *args, **kwargs) -> Any:
+        """Returns the output after storing its input and output keys"""
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return callcounter
+
 
 class Cache:
     """Create Cache class"""
